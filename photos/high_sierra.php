0e69f4ab921caf325fbb6e5e5245a42bd959b06b
Souren <souren@papazian.info>
2016-08-24 13:49:40 -0700

    high sierra

---
+++ photos/high_sierra.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> high sierra </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("../menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <p> High Sierra Trail 2016 </p>
+          <br>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/1.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/2.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/3.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/4.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/5.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/6.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/7.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/8.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/9.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/10.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/11.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+        </div>
+      </div>
+
+    </body>
+</html>
