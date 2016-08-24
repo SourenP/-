5885064d08342ae167b41247b0ca1f4e75408fc9
Souren <souren@papazian.info>
2016-08-24 16:49:06 -0700

    added videos

---
+++ videos/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> videos </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("../menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <div class='img-container'>
+            <iframe src="https://player.vimeo.com/video/175800712" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <br>
+          <div class='img-container'>
+            <iframe src="https://player.vimeo.com/video/138535286" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <br>
+          <div class='img-container'>
+            <iframe src="https://player.vimeo.com/video/176426168" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+
+        </div>
+      </div>
+
+    </body>
+</html>
