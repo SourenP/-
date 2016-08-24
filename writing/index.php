a396842dd5903a949f94bacd74afd61394bacc26
Souren <souren@papazian.info>
2016-08-24 16:30:22 -0700

    added writing

---
+++ writing/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> writing </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("../menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <ul>
+            <li><a href='http://xn--eo8h.ws/writing/villains_concert.php'>Villain's Concert</a></li>
+            <li><a href='http://xn--eo8h.ws/writing/.php'>Artsakh</a></li>
+          </ul>
+
+        </div>
+      </div>
+
+    </body>
+</html>
---
+++ writing/villains_concert.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> february 2016 </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("../menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <div class='writing'>
+            <p> <i> Meant to be read while listening to Trio Eligiaque No.1 in G Minor by Sergei Rachmaninoff. </i> </p>
+
+            <p> He looked like a villain. From somewhere in Eastern Europe, with fair skin and well-kept hair - long, straight and always combed to the left. He would always dress tidily and his navy socks would show in the small gap between his pants and glistening sharp dress shoes. </p>
+            <p> He played cello and was currently absorbed in playing his part in Rachmaninoff’s Trio Eligiaque No.1 in G Minor. The trio had found a certain chemistry, and the exploration of their combined talents had led to a concert at Carnegie Hall a year later. They weren't performing on one of the bigger stages that are meant for an unfamiliar audience; rather they had been expected to fill a humble, small theatre with enough seats for friends and family but not much more. Friends and family who would yet again be amazed at how someone so familiar could induce feelings that were so foreign. Feelings about something abstract, which bothered them more than feelings produced by concrete, first hand experiences. Feelings that didn't matter to them, that were unrelated to them. Feelings that weren't about them, but felt as though they should be. </p>
+          </div>
+        </div>
+      </div>
+
+    </body>
+</html>
