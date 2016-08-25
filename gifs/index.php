116df911fc97a6a60d7d8aa3650438fc2e4c5e31
Souren <souren@papazian.info>
2016-08-24 17:19:28 -0700

    added gifs

---
+++ gifs/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> gifs </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("../menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <p> Creations </p>
+          <br>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/gifs/images/aren-compressed.gif'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/gifs/images/poor1-compressed.gif'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/gifs/images/windsuite-compressed.gif'/><p>&nbsp;</p></div>
+          </div>
+
+          <p> Captures </p>
+          <br>
+
+        </div>
+      </div>
+
+    </body>
+</html>
