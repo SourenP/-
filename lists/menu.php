82247c435963ff3677dcd74e0c4d8343d013891a
Souren <souren@papazian.info>
2016-08-24 18:30:03 -0700

    hotfix

---
+++ lists/menu.php
@@ -0,0 +1,35 @@
+<meta name="viewport" content="width=900px, initial-scale=0">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67798428-2', 'auto');
+  ga('send', 'pageview');
+</script>
+<meta name="description" content="Souren Papazian's website">
+<meta name="keywords" content=" Souren, Papazian, Website, Photography, Writing, Lists, Videos, Gifs, Portfolio">
+
+<div id='menu'>
+  <div class='container'>
+
+    <!--<p><img src='./images/peco.png' id="profile"><br/></p>-->
+
+    <ul>
+      <li><a href='http://xn--eo8h.ws/index.php'>home</a></li>
+      <li><a href='http://xn--eo8h.ws/photos'>photos</a></li>
+      <li><a href='http://xn--eo8h.ws/writing'>writing</a></li>
+      <li><a href='http://xn--eo8h.ws/videos'>videos</a></li>
+      <li><a href='http://xn--eo8h.ws/gifs'>gifs</a></li>
+      <li><a href='http://xn--eo8h.ws/projects'>projects</a></li>
+      <li><a href='http://xn--eo8h.ws/lists'>lists</a></li>
+      <li><a href='http://xn--eo8h.ws/misc'>misc</a></li>
+      <br>
+      <a href="http://instagram.com/youngtableau" target="_blank">instagram</a><br>
+      <a href="http://github.com/sourenp" target="_blank">github</a><br>
+      <a href="mailto:souren@papazian.info">email</a></div>
+    </ul>
+
+  </div>
+</div>
