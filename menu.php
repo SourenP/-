91f7b5f20c22a308bb59631364c4f3552fa749ce
Souren <souren@papazian.info>
2016-08-24 18:29:26 -0700

    projects

--- menu.php
+++ menu.php
@@ -22,7 +22,8 @@
       <li><a href='http://xn--eo8h.ws/writing'>writing</a></li>
       <li><a href='http://xn--eo8h.ws/videos'>videos</a></li>
       <li><a href='http://xn--eo8h.ws/gifs'>gifs</a></li>
-      <li><a href='http://xn--eo8h.ws/lists.php'>lists</a></li>
+      <li><a href='http://xn--eo8h.ws/projects'>projects</a></li>
+      <li><a href='http://xn--eo8h.ws/lists'>lists</a></li>
       <li><a href='http://xn--eo8h.ws/misc'>misc</a></li>
       <br>
       <a href="http://instagram.com/youngtableau" target="_blank">instagram</a><br>
---
+++ projects/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> projects </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("./menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <ul>
+            <li><a target="_blank" href='https://vortan.github.io/'>Vortan</a></li>
+            <li><a target="_blank" href='https://github.com/apollo-lang/apollo'>Appolo</a></li>
+            <li><a target="_blank" href='http://dzoor.com/'>Dzoor</a></li>
+          </ul>
+
+        </div>
+      </div>
+
+    </body>
+</html>
