7868bae0855569589ff165fbce40f9dfc087ce0b
Souren <souren@papazian.info>
2016-09-05 00:49:03 -0700

    hotfix

--- writing/three_men.php
+++ writing/three_men.php
@@ -19,8 +19,8 @@
             <p> I was glad to leave the room. Sharing it with three men in their late forties for no particular reason had started to make me feel uncomfortable and somewhat nervous. </p>
             <p> I left to the kitchen and grabbed our teapot. It was a very geometric pot: various polygons made of rusty metal patched together with just enough force to be able to hold a body of boiling water in place. It had proved to be practical in the past, but I couldn't get the polygons to stay in place this time and the pot kept falling apart. The men didn't really want tea anyway, or at least hadn't expressed that they did, so I wasn't too worried when I returned empty handed. </p>
             <p> I returned to the room to find an Asian woman with her four year old son standing near the entrance to our dorm. I got a similar feeling to when the three men entered the room: I was confused but not surprised, because they were most likely visiting for a reason. Our dorm is tiny so I had nowhere to seat my new guests but my own bed. The mother sat down peacefully and her child started bouncing on my bed. </p>
-
           </div>
+          
         </div>
       </div>
 
