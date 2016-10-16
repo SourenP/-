f1ed91c13187373bfd7eb89e89bbeb156fc9a105
Souren <souren@papazian.info>
2016-10-16 02:21:47 -0700

    added more high sierra

--- photos/high_sierra.php
+++ photos/high_sierra.php
@@ -36,6 +36,10 @@
           </div>
 
           <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/14.png'/><p>&nbsp;</p></div>
+          </div>
+ 
+          <div class='photo-container'>
             <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/6.jpg'/><p>&nbsp;</p></div>
           </div>
 
@@ -56,11 +60,19 @@
           </div>
 
           <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/13.png'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
             <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/11.jpg'/><p>&nbsp;</p></div>
           </div>
 
           <div class='photo-container'>
             <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/12.jpg'/><p>&nbsp;</p></div>
+          </div>
+
+          <div class='photo-container'>
+            <div><img src='http://xn--eo8h.ws/photos/images/high_sierra/15.png'/><p>&nbsp;</p></div>
           </div>
 
         </div>
