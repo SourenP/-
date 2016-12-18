e5a2fb0b79c5b5d5d141c7dc357541aa60e43a48
Souren <souren@papazian.info>
2016-12-18 14:05:10 -0800

    Lotsa things yo

---
+++ snaps/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> snaps </title>
+        <link rel='stylesheet' type='text/css' href='http://xn--eo8h.ws/stylesheet.css'/>
+	      <link rel="shortcut icon" type="image/gif" href="http://xn--eo8h.ws/images/pig.gif"/>
+    </head>
+    <body class='section-1'>
+
+      <?php include("../menu.php"); ?>
+
+      <div id='content'>
+        <div class='container'>
+
+          <div class='photo-container'>
+            Testo
+          </div>
+
+        </div>
+      </div>
+
+    </body>
+</html>
