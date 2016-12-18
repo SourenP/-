5d9a53c69707a5ad1e9e12f3f55933c4dfd7deb2
Souren <souren@papazian.info>
2016-12-18 14:40:31 -0800

    snaps

--- snaps/index.php
+++ snaps/index.php
@@ -12,8 +12,27 @@
       <div id='content'>
         <div class='container'>
 
-          <div class='photo-container'>
-            Testo
+          <div class='snap-container'>
+            <video width="400" controls loop>
+                <source  src="./images/IMG_1377.mp4" type="video/mp4">
+                Your browser sucks.
+            </video>
+          </div>
+
+
+          <div class='snap-container'>
+            <video width="400" controls loop>
+                <source  src="./images/IMG_1359.mp4" type="video/mp4">
+                Your browser sucks.
+            </video>
+          </div>
+
+
+          <div class='snap-container'>
+            <video width="400" controls loop>
+                <source  src="./images/IMG_1394.mp4" type="video/mp4">
+                Your browser sucks.
+            </video>
           </div>
 
         </div>
