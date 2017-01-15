b825532881a9347ae931630a482133e79b107ada
Souren <souren@papazian.info>
2017-01-15 13:01:23 -0800

    Fix menu

--- menu.php
+++ menu.php
@@ -20,9 +20,10 @@
     <ul>
       <li><a href='http://xn--eo8h.ws/index.php'>home</a></li>
       <li><a href='http://xn--eo8h.ws/photos'>photos</a></li>
-      <li><a href='http://xn--eo8h.ws/writing'>writing</a></li>
-      <li><a href='http://xn--eo8h.ws/videos'>videos</a></li>
       <li><a href='http://xn--eo8h.ws/projects'>projects</a></li>
+      <li><a href='http://xn--eo8h.ws/videos'>videos</a></li>
+      <li><a href='http://xn--eo8h.ws/snaps'>snaps</a></li>
+      <li><a href='http://xn--eo8h.ws/writing'>writing</a></li>
       <li><a href='http://xn--eo8h.ws/gifs'>gifs</a></li>
       <li><a href='http://xn--eo8h.ws/lists'>lists</a></li>
       <li><a href='http://xn--eo8h.ws/misc'>misc</a></li>
